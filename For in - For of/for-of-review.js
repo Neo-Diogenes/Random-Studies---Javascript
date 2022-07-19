9f6b831016f8891f8ad54efec17b7317befc246a
Neo-Diogenes <klebs123.ss@gmail.com>
2022-07-19 16:34:46 -0300

    edição

---
+++ For in - For of/for-of-review.js
@@ -0,0 +1,24 @@
+
+
+//FOR IN
+
+// const pessoa = {
+//     nome: 'Jhonatan',
+//     idade: 25
+// }
+
+// for (const key in pessoa) {
+//     console.log(key,pessoa.nome)
+// }
+
+
+const cores = ['vermelho', 'azul', 'verde'];
+
+// for(let indice in cores){
+
+//     console.log(cores[indice])
+// }
+
+for (let teste of cores){
+    console.log(teste)
+}
---
+++ Refazendo a tabuada/Por mim mesmo/script.js
@@ -0,0 +1,25 @@
+
+
+function calcular() {
+let num = document.querySelector('.numero')
+let tab = document.querySelector('.tab')
+
+if(num.value.length == 0){
+    alert('ponha um número')
+} else {
+    var n = Number(num.value)
+    var c = 1
+    tab.innerHTML=""
+
+    for(i=c; i<=10; i++){
+        let item = document.createElement('option')
+        item.innerText = `${n} x ${i} = ${n*i}`
+        tab.appendChild(item)
+
+
+
+
+    }
+}
+
+}

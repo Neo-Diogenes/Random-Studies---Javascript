09896fdc34aa92011382a4e9cc45e1b7fd3b052c
Neo-Diogenes <klebs123.ss@gmail.com>
2022-07-27 19:47:44 -0300

    revisão

--- CFB-Cursos/29-funções.js
+++ CFB-Cursos/29-funções.js
@@ -2,14 +2,17 @@
 
 var numeros = [10,50,5,30,25,19,20,50];
 
-function media(numeros){
-    var tam = numeros.length;
+function media(numbers){
     var soma = 0;
-    for(var i=0; i<tam; i++){
-        soma+=numeros[i];
+    for(var i=0; i<numbers.length; i++){
+        soma+=numbers[i];
     }
-    return Math.round(soma/tam);
+    return Math.round(soma/numbers.length);
 }
 
 console.log(media(numeros));
 
+
+
+// ele criou uma váriavel para o numeros.length
+//fiz da minha forma
